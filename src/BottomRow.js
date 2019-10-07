909a0d6b08dcc51ef27a9d1ade5fc1fb94abe09e
Addison Hill <adddison.hill@gmail.com>
2019-10-07 15:10:47 -0500

    added functionality to bottom row

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,28 +1,36 @@
 import React, {useState} from "react";
 import "./App.css";
 
-// const [quarterCount, quarterSetCount] = useState(0);
-
 const BottomRow = () => {
+  const [quarterCount, quarterSetCount] = useState(0);
+  const [downCount, downSetCount] = useState(0);
+  const [togoCount, togoSetCount] = useState(0);
+  const [ballOnCount, ballOnSetCount] = useState(0);
   return (
+    <>
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{downCount}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">{togoCount}</div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">{ballOnCount}</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{quarterCount}</div>
       </div>
     </div>
+    <button className="homeButtons__touchdown" onClick={() => downSetCount(downCount + 1)}>Next Down</button>
+    <button className="homeButtons__touchdown" onClick={() => togoSetCount(togoCount + 1)}>Add 1   </button>
+    <button className="homeButtons__touchdown" onClick={() => ballOnSetCount(ballOnCount + 1)}>Add 1   </button>
+    <button className="homeButtons__touchdown" onClick={() => quarterSetCount(quarterCount + 1)}>Next Quarter</button>
+    </>
   );
 };
 
